a5daf54180f39251dea124d7114ebfe501998ae7
Natalie Cooper <ncooper@tcd.ie>
2014-03-25 13:21:39 +0000

    Added overall function for SPELT plus checks for data types

--- SPELT_functions.R
+++ SPELT_functions.R
@@ -128,23 +128,23 @@ add.SPELT.contrasts.data <- function(SPELT.data) {
 }
 
 # Remove branches shorter than user defined age limit
-remove.young.branches <- function(SPELT.data, age.limit = NULL) {
+remove.young.branches <- function(SPELT.data, age.limit = NULL, cut.off = 3) {
   if (!is.null(age.limit)) {
     SPELT.data <- SPELT.data[-(c(which(SPELT.data$branch.length < age.limit))), ]
   }
-    if (nrow(SPELT.data) < 3) {
-      stop("< 3 branches longer than age limit")
+    if (nrow(SPELT.data) < cut.off) {
+      stop(paste("< ", cut.off, " branches longer than age limit"))
     }
   return(SPELT.data) 
 }
 
 # Combining all functions for data collation
 get.SPELT.data <- function(phy, data, node.list, var1.col, var2.col, 
-                           SPELT.data, age.limit = NULL) {
+                           SPELT.data, age.limit = NULL, cut.off) {
   SPELT.data <- build.SPELT.data(phy)
   SPELT.data <- add.SPELT.data(phy, data, node.list, var1.col, var2.col, SPELT.data)
   SPELT.data <- get.raw.contrasts(SPELT.data)
-  SPELT.data <- remove.young.branches(SPELT.data, age.limit)
+  SPELT.data <- remove.young.branches(SPELT.data, age.limit, cut.off)
   SPELT.data <- add.SPELT.contrasts.data(SPELT.data)
   return(SPELT.data)
 }
@@ -154,16 +154,67 @@ fit.lag.model <- function(SPELT.data) {
   lag.model <- lm(SPELT.data$residuals ~ SPELT.data$branch.length)
 }
 
+# Overall SPELT function
+SPELT <- function(phy, data, primary.variable, lag.variable, speciesnames, 
+                  age.limit = NA, warn.dropped = TRUE, cut.off = 3) {
 
+  if (!is.data.frame(data)) 
+    stop("'data' must be an object of class 'data.frame'")
 
+  if (!inherits(phy, "phylo")) 
+        stop("'phy' must be an object of class 'phylo'")
 
+  # Define variables
+  var1.col <- column.ID(data, primary.variable)
+  if (length(var1.col) == 0)
+    stop("Primary variable not found in data")  
+  
+  var2.col <- column.ID(data, lag.variable)
+  if (length(var2.col) == 0)
+    stop("Lag variable not found in data")
+  
+  speciesnames.col <- column.ID(data, speciesnames)
+  if (length(speciesnames.col) == 0)
+    stop("Species names not found in data")
 
+  # Ensure phylogeny has no polytomies
+  phy <- multi2di(phy)
+  
+  # Tidy up data and tree
+  data <- remove.incomplete.data(data, var1.col, var2.col)
+  data <- remove.missing.species.data(phy, data, speciesnames.col)
+  if (nrow(data) < cut.off)
+    stop(paste("< ", cut.off, " species have data for both variables and are in the phylogeny"))
+  phy <- remove.missing.species.tree(phy, data, speciesnames.col)
 
+  if (warn.dropped) {
+    tree.not.data <- id.missing.tree(phy, data, speciesnames.col)
+    data.not.tree <- id.missing.data(phy, data, speciesnames.col)
+  }
 
+  # Identify cherries (independent pairs of species from one node)
+  node.list <- cherry.nodes(phy)
+  
+  # Collate data required for SPELT analyses
+  SPELT.data <- get.SPELT.data(phy, data, node.list, 
+                               var1.col, var2.col, SPELT.data, age.limit, cut.off)
+
+  # Fit SPELT model 
+  SPELT.model <- fit.lag.model(SPELT.data)
+  
+  # Outputs
+  SPELT.results <- list(summary = summary(SPELT.model), variables = list(primary.variable = primary.variable, 
+                        lag.variable = lag.variable), data = SPELT.data, age.limit = age.limit,
+                        dropped = list(tree.not.data = tree.not.data, data.not.tree = data.not.tree), 
+                        Nnodes = nrow(SPELT.data)) 
+
+  class(SPELT.results) <- "SPELT"
+  return(SPELT.results)
+}
 
 # Collate details for summary and plot outputs
 SPELT.summary.details <- function(SPELT.results) {
-  details <- paste("SPELT results: primary variable = ", SPELT.results$variables$primary.variable,
+  details <- paste("primary variable = ", SPELT.results$variables$primary.variable,
                       ", lag variable = ", SPELT.results$variables$lag.variable, sep = "")
   if (!is.null(SPELT.results$age.limit)) {
     age.limit <- paste("age limit = ", SPELT.results$age.limit, sep = "")
@@ -178,8 +229,8 @@ plot.SPELT <- function(SPELT.results) {
   par(bty = "l")
   plot(SPELT.results$data$residuals ~ SPELT.results$data$branch.length, 
        xlab = paste("divergence time (", SPELT.summary.details(SPELT.results)[[2]],")", sep = ""),
-       ylab = "residuals", main = SPELT.summary.details(SPELT.results)[[1]], 
-       cex.main = 0.8, pch = 16, las = 1)
+       ylab = "residuals", cex.main = 0.8, pch = 16, las = 1,
+       main = paste("SPELT results: ", SPELT.summary.details(SPELT.results)[[1]], sep = ""))
   abline(fit.lag.model(SPELT.results$data))
   abline(0,0,lty = 2)
 }  
@@ -190,5 +241,3 @@ summary.SPELT <- function(SPELT.results) {
   cat("\n",SPELT.summary.details(SPELT.results)[[2]], "\n")
   print(SPELT.results$summary)
 }
-
-
