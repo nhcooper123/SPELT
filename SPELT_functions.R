f97ebe471294b4df8d0db5a4c6f421c04d153e7f
Natalie Cooper <ncooper@tcd.ie>
2014-03-25 09:57:45 +0000

    Tidied up functions according to Google R Style guide

--- SPELT_functions.R
+++ SPELT_functions.R
@@ -1,95 +1,93 @@
-#Functions required for SPELT method for detecting evolutionary lag
-#Based on Deaner and Nunn 1999
+# Functions required for SPELT method for detecting evolutionary lag
 
-#Identify column numbers
+# Identify column numbers
 column.ID <- function(data, column.name) { 
-  which(names(data)==column.name)
+  which(names(data) == column.name)
 }	
  
-#Remove incomplete data
+# Remove incomplete data
 remove.incomplete.data <- function(data, var1.col, var2.col) {
-  id <- complete.cases(data[,c(var1.col,var2.col)])
-  data <- data[id,]
+  id <- complete.cases(data[, c(var1.col, var2.col)])
+  data <- data[id, ]
   return(data)
 }
 
-#ID species in tree that are not in the data
+# ID species in tree that are not in the data
 id.missing.tree <- function(phy, data, speciesnames.col) {
-  setdiff(phy$tip.label, data[,speciesnames.col])
+  setdiff(phy$tip.label, data[, speciesnames.col])
 }
 
-#ID species in data that are not in the tree
+# ID species in data that are not in the tree
 id.missing.data <- function(phy, data, speciesnames.col) {
-  setdiff(data[,speciesnames.col], phy$tip.label)
+  setdiff(data[, speciesnames.col], phy$tip.label)
 }    
 
-#Remove missing species from tree
+# Remove missing species from tree
 remove.missing.species.tree <- function(phy, data, speciesnames.col) {
   tree.not.data <- id.missing.tree(phy, data, speciesnames.col)
-  if(length(tree.not.data)>0) {
+  if (length(tree.not.data) > 0) {
     phy <- drop.tip(phy, tree.not.data)
-  } else {
-    phy <- phy
   }
   return(phy)
 }
 
-#Remove missing species from data
+# Remove missing species from data
 remove.missing.species.data <- function(phy, data, speciesnames.col) {
   data.not.tree <- id.missing.data(phy, data, speciesnames.col)
-  if(length(data.not.tree)>0) {
-    matches <- match(data[,speciesnames.col], data.not.tree, nomatch =0)
-    data <- subset(data, matches==0)
-  } else {
-    data <- data
+  if (length(data.not.tree) > 0) {
+    matches <- match(data[,speciesnames.col], data.not.tree, nomatch = 0)
+    data <- subset(data, matches == 0)
   }
   return(data)
 }
 
-#Identify total number of nodes in tree
+# Identify total number of nodes in tree
 total.nodes <- function(phy) {
   length(phy$tip.label)
 }
 
-#Identify cherries (independent pairs of species from one node)
+# Identify cherries (independent pairs of species from one node)
 cherry.nodes <- function(phy) {
-  names(which(table(phy$edge[,1][phy$edge[,2]<=total.nodes(phy)])==2))
+  names(which(table(phy$edge[, 1][phy$edge[, 2]<=total.nodes(phy)]) == 2))
 }
 
-#Identify 1st species coming from node
+# Identify 1st species coming from node
 node.species1 <- function(phy, node.list) {
-  sapply(node.list, function(x) phy$tip.label[phy$edge[,2][which(phy$edge[,1]==x)][1]])
+  sapply(node.list, function(x) 
+         phy$tip.label[phy$edge[, 2][which(phy$edge[, 1] == x)][1]])
 }
 
-#Identify 2nd species coming from node
+# Identify 2nd species coming from node
 node.species2 <- function(phy, node.list) {
-  sapply(node.list, function(x) phy$tip.label[phy$edge[,2][which(phy$edge[,1]==x)][2]])
+  sapply(node.list, function(x) 
+         phy$tip.label[phy$edge[, 2][which(phy$edge[, 1] == x)][2]])
 }
 
-#Extract data for species
+# Extract data for species
 get.data <- function(data, variable.col, species.list) {
-  sapply(species.list, function(x) data[which(rownames(data)==x),variable.col])
+  sapply(species.list, function(x) 
+         data[which(rownames(data) == x),variable.col])
 }
 
-#Extract branch length for contrast between two species
-#Need a fix for polytomies!!! == numeric(0)
+# Extract branch length for contrast between two species
+# Need a fix for polytomies!!! == numeric(0)
 branch.length.pair <- function(node.list) {
   sapply(node.list, function(x) phy$edge.length[which(phy$edge[,1]==x)][1])
 }
 
-#Build empty dataset for SPELT
+# Build empty dataset for SPELT
 build.SPELT.data <- function(phy) {
   SPELT.data <- data.frame(array(dim = c(length(cherry.nodes(phy)),10)))
-#  colnames(SPELT.data)<-c("species1", "species2", "species1.primary.var", 
-#                       "species2.primary.var", "species1.lag.var",
-#                       "species2.lag.var", "branch.length", 
-#                       "contrast.primary.var", "contrast.lag.var", 
-#                       "residuals")
+  colnames(SPELT.data)<-c("species1", "species2", "species1.var1", 
+                      "species2.var1", "species1.var2",
+                       "species2.var2", "branch.length", 
+                      "contrast.var1", "contrast.var2", 
+                       "residuals")
 }
 
-#Calculate contrasts (primary variable contrast is always positive)
+# Calculate contrasts (primary variable contrast is always positive)
 get.raw.contrasts <- function(SPELT.data) { 
-  for(i in seq_along(SPELT.data[,1])) {
+  for(i in seq_along(SPELT.data$)) {
     if(SPELT.data[i,3] > SPELT.data[i,4]) {
       SPELT.data[i,8] <- SPELT.data[i,3] - SPELT.data[i,4]
       SPELT.data[i,9] <- SPELT.data[i,5] - SPELT.data[i,6]
@@ -101,7 +99,7 @@ get.raw.contrasts <- function(SPELT.data) {
   return(SPELT.data)
 }
 
-#Fill data set for SPELT
+# Fill data set for SPELT
 add.SPELT.data <- function(phy, data, node.list, var1.col, var2.col, SPELT.data) {
   SPELT.data[,1] <- node.species1(phy,node.list)
   SPELT.data[,2] <- node.species2(phy,node.list)
@@ -113,16 +111,16 @@ add.SPELT.data <- function(phy, data, node.list, var1.col, var2.col, SPELT.data)
   get.raw.contrasts(SPELT.data)
 }
 
-#Fit model of lag variable contrasts ~ primary variable contrasts (forced through the origin)
+# Fit model of lag variable contrasts ~ primary variable contrasts (forced through the origin)
 contrasts.model.residuals <- function(SPELT.data) {
-  model<-lm(SPELT.data[,9] ~ SPELT.data[,8]-1)
+  model<-lm(SPELT.data[,9] ~ SPELT.data[,8] - 1)
   residuals<-unclass(model)$residuals
   return(residuals)
 }
 
 #Fit model of redisuals against divergence time for each species pair
 lag.model <- function(SPELT.data) {
-  lag.model<-lm(SPELT.data[,10] ~ SPELT.data[,7])
+  lag.model <- lm(SPELT.data[,10] ~ SPELT.data[,7])
 }
 
 #Plotting function for SPELT objects
