0f5c8b14bec959aabe6483cf764e9e08d4a4d5dc
Natalie Cooper <ncooper@tcd.ie>
2014-03-25 10:48:22 +0000

    Changed data collation functions to use column names not numbers

--- SPELT_functions.R
+++ SPELT_functions.R
@@ -26,7 +26,7 @@ id.missing.data <- function(phy, data, speciesnames.col) {
 remove.missing.species.tree <- function(phy, data, speciesnames.col) {
   tree.not.data <- id.missing.tree(phy, data, speciesnames.col)
   if (length(tree.not.data) > 0) {
-    phy <- drop.tip(phy, tree.not.data)
+     phy <- drop.tip(phy, tree.not.data)
   }
   return(phy)
 }
@@ -35,8 +35,8 @@ remove.missing.species.tree <- function(phy, data, speciesnames.col) {
 remove.missing.species.data <- function(phy, data, speciesnames.col) {
   data.not.tree <- id.missing.data(phy, data, speciesnames.col)
   if (length(data.not.tree) > 0) {
-    matches <- match(data[,speciesnames.col], data.not.tree, nomatch = 0)
-    data <- subset(data, matches == 0)
+     matches <- match(data[,speciesnames.col], data.not.tree, nomatch = 0)
+     data <- subset(data, matches == 0)
   }
   return(data)
 }
@@ -72,58 +72,76 @@ get.data <- function(data, variable.col, species.list) {
 # Extract branch length for contrast between two species
 # Need a fix for polytomies!!! == numeric(0)
 branch.length.pair <- function(node.list) {
-  sapply(node.list, function(x) phy$edge.length[which(phy$edge[,1]==x)][1])
+  sapply(node.list, function(x) 
+         phy$edge.length[which(phy$edge[,1] == x)][1])
 }
 
 # Build empty dataset for SPELT
 build.SPELT.data <- function(phy) {
-  SPELT.data <- data.frame(array(dim = c(length(cherry.nodes(phy)),10)))
-  colnames(SPELT.data)<-c("species1", "species2", "species1.var1", 
-                      "species2.var1", "species1.var2",
-                       "species2.var2", "branch.length", 
-                      "contrast.var1", "contrast.var2", 
-                       "residuals")
+  SPELT.data <- data.frame(array(dim = c(length(cherry.nodes(phy)), 10)))
+  colnames(SPELT.data) <- c("species1", "species2", "species1.var1", 
+                            "species2.var1", "species1.var2",
+                            "species2.var2", "branch.length", 
+                            "contrast.var1", "contrast.var2", 
+                            "residuals")
+  return(SPELT.data)
+}
+
+# Add species and variable data into SPELT dataset
+add.SPELT.data <- function(phy, data, node.list, var1.col, var2.col, SPELT.data) {
+  SPELT.data$species1 <- node.species1(phy,node.list)
+  SPELT.data$species2 <- node.species2(phy,node.list)
+  SPELT.data$species1.var1 <- get.data(data, var1.col, SPELT.data$species1)
+  SPELT.data$species2.var1 <- get.data(data, var1.col, SPELT.data$species2)
+  SPELT.data$species1.var2 <- get.data(data, var2.col, SPELT.data$species1)
+  SPELT.data$species2.var2 <- get.data(data, var2.col, SPELT.data$species2)
+  SPELT.data$branch.length <- branch.length.pair(node.list)
+  return(SPELT.data)
 }
 
 # Calculate contrasts (primary variable contrast is always positive)
+# And add into SPELT dataset
 get.raw.contrasts <- function(SPELT.data) { 
-  for(i in seq_along(SPELT.data$)) {
-    if(SPELT.data[i,3] > SPELT.data[i,4]) {
-      SPELT.data[i,8] <- SPELT.data[i,3] - SPELT.data[i,4]
-      SPELT.data[i,9] <- SPELT.data[i,5] - SPELT.data[i,6]
+  for(i in seq_along(SPELT.data$species1)) {
+    if (SPELT.data$species1.var1[i] >= SPELT.data$species2.var1[i]) {
+      SPELT.data$contrast.var1[i] <- SPELT.data$species1.var1[i] - SPELT.data$species2.var1[i]
+      SPELT.data$contrast.var2[i] <- SPELT.data$species1.var2[i] - SPELT.data$species2.var2[i]
     } else {
-      SPELT.data[i,8] <- SPELT.data[i,4] - SPELT.data[i,3]
-      SPELT.data[i,9] <- SPELT.data[i,6] - SPELT.data[i,5]
+      SPELT.data$contrast.var1[i] <- SPELT.data$species2.var1[i] - SPELT.data$species1.var1[i]
+      SPELT.data$contrast.var2[i] <- SPELT.data$species2.var2[i] - SPELT.data$species1.var2[i]
     }
   }  
   return(SPELT.data)
 }
 
-# Fill data set for SPELT
-add.SPELT.data <- function(phy, data, node.list, var1.col, var2.col, SPELT.data) {
-  SPELT.data[,1] <- node.species1(phy,node.list)
-  SPELT.data[,2] <- node.species2(phy,node.list)
-  SPELT.data[,3] <- get.data(data, var1.col, SPELT.data[,1])
-  SPELT.data[,4] <- get.data(data, var1.col, SPELT.data[,2])
-  SPELT.data[,5] <- get.data(data, var2.col, SPELT.data[,1])
-  SPELT.data[,6] <- get.data(data, var2.col, SPELT.data[,2])
-  SPELT.data[,7] <- branch.length.pair(node.list)
-  get.raw.contrasts(SPELT.data)
-}
-
 # Fit model of lag variable contrasts ~ primary variable contrasts (forced through the origin)
 contrasts.model.residuals <- function(SPELT.data) {
-  model<-lm(SPELT.data[,9] ~ SPELT.data[,8] - 1)
-  residuals<-unclass(model)$residuals
+  model <- lm(SPELT.data$contrast.var2 ~ SPELT.data$contrast.var1 - 1)
+  residuals <- unclass(model)$residuals
   return(residuals)
 }
 
-#Fit model of redisuals against divergence time for each species pair
-lag.model <- function(SPELT.data) {
-  lag.model <- lm(SPELT.data[,10] ~ SPELT.data[,7])
+# Add contrasts residuals to SPELT dataset
+add.SPELT.contrasts.data <- function(SPELT.data) {
+  SPELT.data$residuals <- contrasts.model.residuals(SPELT.data)
+  return(SPELT.data)
 }
 
-#Plotting function for SPELT objects
+# Combining all functions for data collation
+get.SPELT.data <- function(phy, data, node.list, var1.col, var2.col, SPELT.data) {
+  SPELT.data <- build.SPELT.data(phy)
+  SPELT.data <- add.SPELT.data(phy, data, node.list, var1.col, var2.col, SPELT.data)
+  SPELT.data <- get.raw.contrasts(SPELT.data)
+  SPELT.data <- add.SPELT.contrasts.data(SPELT.data)
+  return(SPELT.data)
+}
+
+# Fit model of residuals against divergence time for each species pair
+fit.lag.model <- function(SPELT.data) {
+  lag.model <- lm(SPELT.data$residuals ~ SPELT.data$branch.length)
+}
+
+# Plotting function for SPELT objects
 plot.SPELT <- function(SPELT.results) {
   plot(SPELT.data[,10] ~ SPELT.data[,7], xlab = "divergence time", 
        ylab = "residuals", main = "SPELT plot", pch = 16)
